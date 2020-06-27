6bee02900a00fa2890467d47bed8de7ebd7bcc02
Hayato Mitomi <mh.88103103@gmail.com>
2020-06-28 01:21:44 +0900

    プレイヤーを選択する機能の追加

--- app/src/main/java/com/forest/bombgame/MainActivity2.java
+++ app/src/main/java/com/forest/bombgame/MainActivity2.java
@@ -7,14 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    private SelectPlayerLogic selectPlayerLogic;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        selectPlayerLogic = new SelectPlayerLogic(4);
+        textView = findViewById(R.id.selectedPlayerView);
         final Button button2 = findViewById(R.id.secondButton);
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -23,5 +27,14 @@ public class MainActivity2 extends AppCompatActivity {
                 Log.d("debug", "button2, Perform action on click");
             }
         });
+        int nextPlayer = selectPlayerLogic.getNextPlayer();
+        if (nextPlayer == -1) {
+            Log.d("debug", "error failed to select player");
+            while (nextPlayer == -1) {
+                nextPlayer = selectPlayerLogic.getNextPlayer();
+            }
+        }
+        textView.setText("Player" + String.valueOf(nextPlayer));
     }
+
 }
---
+++ app/src/main/java/com/forest/bombgame/SelectPlayerLogic.java
@@ -0,0 +1,36 @@
+package com.forest.bombgame;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+public class SelectPlayerLogic {
+    private static ArrayList<Integer>players=null;
+    private static int sum;
+    public SelectPlayerLogic(int playerNumber){
+        if(players==null||players.size()!=playerNumber) {
+            SelectPlayerLogic.sum = playerNumber;
+            SelectPlayerLogic.players = new ArrayList<>();
+            for (int i = 0; i < playerNumber; i++) {
+                players.add(1);
+            }
+        }
+    }
+    public int getNextPlayer(){
+        Random r=new Random();
+        int randomNum=r.nextInt(sum)+1;
+        int res=-1;
+        for(int i=0;i<players.size();i++){
+            randomNum-=players.get(i);
+            if(randomNum<=0){
+                res=i+1;
+                for(int j=0;j<players.size();j++){
+                    if(j==i)continue;
+                    players.set(j,players.get(j)+1);
+                }
+                sum+=players.size()-1;
+                break;
+            }
+        }
+        return res;
+    }
+}
