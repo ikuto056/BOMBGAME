5fe5533f25bba1995837dab822fb85e48770cf84
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-06-29 17:12:58 +0900

    テキストの追加と更新処理の実装

--- app/src/main/java/com/forest/bombgame/MainActivity4.java
+++ app/src/main/java/com/forest/bombgame/MainActivity4.java
@@ -3,12 +3,25 @@ package com.forest.bombgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
+    //TODO: 一つ前のアクティビティから値を受け取る処理を実装する
+    int player = 2; //プレイヤーの番号
+    int count = 3;  //爆弾を押す個数
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        setExplainText();
+    }
+
+    //NOTE: 指示テキストの更新処理
+    private void setExplainText() {
+        TextView explainText = findViewById(R.id.terminalViewExplainText);
+        explainText.setText("Player" + String.valueOf(player) + "\n"
+                            + "Press" + String.valueOf(count) + "buttons!");
     }
 }
